6284385ee65d43099fdf0e613f878afacb6646ed
rajeeb <rajeeb@bitmascot.com>
2022-08-19 21:46:59 +0600

    update file

--- index.js
+++ index.js
@@ -1,143 +1,151 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectID = require('mongodb').ObjectID;
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ObjectId } = require("mongodb");
+require("dotenv").config();
 
-const uri = "mongodb+srv://tanveer:tanveer123@cluster0.qsr7x.mongodb.net/doctors-portal?retryWrites=true&w=majority";
+const app = express();
+const port = process.env.PORT || 8000;
 
-const app = express()
-
-app.use(express.json());
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.14pyq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 app.use(cors());
-app.use(express.static('doctors'));
-app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const port = 5000;
-
-app.get('/', (req, res) => {
-    console.log('database is working');
-})
-
-const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-client.connect(err => {
-    const appointmentCollection = client.db("doctors-portal").collection("appointments");
-    const doctorCollection = client.db("doctors-portal").collection("doctors");
-    const reviewCollection = client.db("doctors-portal").collection("reviews");
-    const serviceCollection = client.db("doctors-portal").collection("service");
-
-    console.log('database connected')
-
-    app.post('/addAppointment', (req, res) => {
-        const appointment = req.body;
-        appointmentCollection.insertOne(appointment)
-            .then(result => {
-                res.send(result.insertedCount > 0)
-            })
-    });
-
-    app.get('/appointments', (req, res) => {
-        appointmentCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-    })
-
-    app.post('/appointmentsByDate', (req, res) => {
-        const date = req.body;
-        const email = req.body.email;
-        doctorCollection.find({email: email})
-            .toArray((err, doctors) => {
-                const filter = {date: date.date}
-                if (doctors.length === 0) {
-                    filter.email = email;
-                }
-                appointmentCollection.find(filter)
-                    .toArray((err, documents) => {
-                        console.log(email, date.date, doctors, documents)
-                        res.send(documents);
-                    })
-            })
-    })
-
-    app.post('/addAdmin', (req, res) => {
-        const file = req.files.file;
-        const name = req.body.name;
-        const email = req.body.email;
-        const newImg = file.data;
-        const encImg = newImg.toString('base64');
-
-        let image = {
-            contentType: file.mimetype,
-            size: file.size,
-            img: Buffer.from(encImg, 'base64')
-        };
-
-        doctorCollection.insertOne({name, email, image})
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
-    app.post('/addService', (req, res) => {
-
-        const bandwidth = req.body.bandwidth;
-        const service_hour = req.body.service_hour;
-        const ip_service = req.body.ip_service;
-        const monthly_charge = req.body.monthly_charge;
-
-        serviceCollection.insertOne({bandwidth, service_hour, ip_service, monthly_charge})
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
-    app.get('/getServices', (req, res) => {
-
-        serviceCollection.find({})
-            .toArray((err, document) => {
-                res.send(document);
-            })
-    })
-
-    app.delete('/deleteService/:id', (req, res) => {
-        serviceCollection.deleteOne({_id: ObjectID(req.params.id)})
-            .then(result => {
-                res.send(result.deletedCount > 0);
-            })
-    })
-
-    app.post('/addReview', (req, res) => {
-
-        const name = req.body.name;
-        const email = req.body.email;
-        const review = req.body.review;
-
-        reviewCollection.insertOne({name, email, review})
-            .then(result => {
-                res.send(result.insertedCount > 0);
-            })
-    })
-    app.get('/admins', (req, res) => {
-        doctorCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-    });
-    app.get('/reviews', (req, res) => {
-        reviewCollection.find({})
-            .toArray((err, documents) => {
-                res.send(documents);
-            })
-    });
-    app.post('/isAdmin', (req, res) => {
-        const email = req.body.email;
-        doctorCollection.find({email: email})
-            .toArray((err, doctors) => {
-                res.send(doctors.length > 0);
-            })
-    })
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(process.env.PORT || port)
+client.connect((err) => {
+  const servicesCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_SERVICES_COLLECTION}`);
+  const reviewsCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_REVIEWS_COLLECTION}`);
+  const bookingCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_BOOKING_COLLECTION}`);
+  const adminCollection = client
+    .db(`${process.env.DB_NAME}`)
+    .collection(`${process.env.DB_ADMIN_COLLECTION}`);
+  const IptvCollection = client
+    .db(`${process.env.DB_Name}`)
+    .collection(`${process.env.DB_IPTV_COlLECTION}`);
+
+  // add Services
+  app.post("/addServices", (req, res) => {
+    const newServices = req.body;
+    servicesCollection.insertOne(newServices).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // delete Services
+  app.delete("/services/delete/:id", (req, res) => {
+    servicesCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+
+  // all services
+  app.get("/services", (req, res) => {
+    servicesCollection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  // add review
+  app.post("/addReviews", (req, res) => {
+    const newReviews = req.body;
+    reviewsCollection.insertOne(newReviews).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // all reviews
+  app.get("/reviews", (req, res) => {
+    reviewsCollection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  // add booking
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    bookingCollection.insertOne(newBooking).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // find bookings
+  app.get("/bookings", (req, res) => {
+    bookingCollection
+      .find({ userEmail: req.query.email })
+      .toArray((err, result) => {
+        res.send(result);
+      });
+  });
+
+  // update booking status
+  app.patch("/bookingUpdate/:id", (req, res) => {
+    bookingCollection
+      .updateOne(
+        { _id: ObjectId(req.params.id) },
+        { $set: { bookingStatus: req.body.updateValue } }
+      )
+      .then((result) => {
+        res.send(result.matchedCount > 0);
+      });
+  });
+
+  // find bookings
+  app.get("/allBookings", (req, res) => {
+    bookingCollection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  // add Iptv
+  app.post("/addIptv", (req, res) => {
+    const ipTv = req.body;
+    IptvCollection.insertOne(ipTv).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // all Iptv
+  app.get("/Iptv", (req, res) => {
+    IptvCollection.find({}).toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  // add admin
+  app.post("/addAdmin", (req, res) => {
+    const newAdmin = req.body;
+    adminCollection.insertOne(newAdmin).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  // find Admin
+  app.get("/admin", (req, res) => {
+    adminCollection.find({ email: req.query.email }).toArray((err, result) => {
+      res.send(result.length > 0);
+    });
+  });
+  err
+    ? console.log("Database Connection Faild!")
+    : console.log("Hurray!! Database Connected Successfully.");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Internet Service Provider");
+});
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
